3e0cf14f488c8d4002748201912138f310738489
Rian Lima <rlimamendes@proton.me>
2024-07-11 22:51:20 -0300

    Created controller to SocialMetaController with method to send url data to client side

---
+++ src/main/java/com/rlima/demo/web/controller/SocialMetaTagController.java
@@ -0,0 +1,23 @@
+package com.rlima.demo.web.controller;
+
+import com.rlima.demo.domain.SocialMetaTag;
+import com.rlima.demo.service.SocialMetaTagService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/meta")
+public class SocialMetaTagController {
+    @Autowired
+    private SocialMetaTagService service;
+
+    @PostMapping("/info")
+    public ResponseEntity<SocialMetaTag> getDataFromURL(@RequestParam("url") String url) {
+        SocialMetaTag socialMetaTag = service.getOpenGraphByURL(url);
+        return socialMetaTag != null ? ResponseEntity.ok(socialMetaTag) : ResponseEntity.notFound().build();
+    }
+}
