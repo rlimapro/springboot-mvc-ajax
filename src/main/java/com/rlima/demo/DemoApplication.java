9529767a231b17a3a58e1881ca51a95d9e5190b7
Rian Lima <rlimamendes@proton.me>
2024-07-17 19:52:51 -0300

    method to capture data from twitter card namespace

--- src/main/java/com/rlima/demo/DemoApplication.java
+++ src/main/java/com/rlima/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.rlima.demo;
 
+import com.rlima.demo.domain.SocialMetaTag;
 import com.rlima.demo.service.SocialMetaTagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -18,5 +19,7 @@ public class DemoApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        SocialMetaTag tag = service.getTwitterCardByURL("https://www.gkinfostore.com.br/vzf0t2wzq-ssd-960gb-a400-kingston-25-sata-iii-blister-sa400s37960g");
+        System.out.println(tag);
     }
 }
--- src/main/java/com/rlima/demo/service/SocialMetaTagService.java
+++ src/main/java/com/rlima/demo/service/SocialMetaTagService.java
@@ -9,6 +9,21 @@ import java.io.IOException;
 
 @Service
 public class SocialMetaTagService {
+    public SocialMetaTag getTwitterCardByURL(String URL) {
+        SocialMetaTag tag = new SocialMetaTag();
+        try {
+            Document doc = Jsoup.connect(URL).userAgent("Mozilla").get();
+            tag.setSite(doc.head().select("meta[name=twitter:site]").attr("content"));
+            tag.setTitle(doc.head().select("meta[name=twitter:title]").attr("content"));
+            tag.setUrl(doc.head().select("meta[name=twitter:url]").attr("content"));
+            tag.setImage(doc.head().select("meta[name=twitter:image]").attr("content"));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return tag;
+    }
+
     public SocialMetaTag getOpenGraphByURL(String URL) {
         SocialMetaTag tag = new SocialMetaTag();
         try {
