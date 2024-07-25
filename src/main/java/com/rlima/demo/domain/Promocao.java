611518567fa6c48fe8009c971e940f1cac9493ba
Rian Lima <rlimamendes@proton.me>
2024-07-24 21:53:39 -0300

    product entity created

---
+++ src/main/java/com/rlima/demo/domain/Promocao.java
@@ -0,0 +1,111 @@
+package com.rlima.demo.domain;
+
+import jakarta.persistence.*;
+import org.springframework.format.annotation.NumberFormat;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "promocoes")
+public class Promocao implements Serializable {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    @Column(name = "link_promocao", nullable = false)
+    private String linkPromocao;
+
+    @Column(name = "site_promocao", nullable = false)
+    private String site;
+
+    private String descricao;
+
+    @Column(name = "link_imagem", nullable = false)
+    private String linkImagem;
+
+    @NumberFormat(style = NumberFormat.Style.CURRENCY, pattern = "#,###.00")
+    @Column(name = "preco_promocao", nullable = false)
+    private BigDecimal preco;
+
+    @Column(name = "total_likes")
+    private int likes;
+
+    @Column(name = "data_cadastro", nullable = false)
+    private LocalDateTime dataCadastro;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getLinkPromocao() {
+        return linkPromocao;
+    }
+
+    public void setLinkPromocao(String linkPromocao) {
+        this.linkPromocao = linkPromocao;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getLinkImagem() {
+        return linkImagem;
+    }
+
+    public void setLinkImagem(String linkImagem) {
+        this.linkImagem = linkImagem;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public LocalDateTime getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDateTime dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+}
