c29faa1184271de97d21ba5e589e4a270efd9296
Rian Lima <rlimamendes@proton.me>
2024-08-13 14:54:37 -0300

    infinite scroll effect finished

--- src/main/resources/static/js/promo-list.js
+++ src/main/resources/static/js/promo-list.js
@@ -1,11 +1,16 @@
 var pageNumber = 0;
 
+$(document).ready(function() {
+    $("#loader-img").hide();
+    $("#fim-btn").hide();
+});
+
 // infinite scroll
 $(window).scroll(function() {
     var scrollTop = $(this).scrollTop();
     var content = $(document).height() - $(window).height();
 
-    console.log('scrollTop: ', scrollTop, " | content: ", content);
+    //console.log('scrollTop: ', scrollTop, " | content: ", content);
 
     if(scrollTop >= content) {
         pageNumber++;
@@ -23,8 +28,28 @@ function loadByScrollBar(pageNumber) {
         data: {
             page: pageNumber
         },
+        beforeSend: function() {
+            $("#loader-img").show();
+        },
         success: function(response) {
-            console.log('resposta > ', response);
+            //console.log('resposta > ', response);
+
+            if(response.length > 150) {
+                $(".row").fadeIn(250, function() {
+                    $(this).append(response);
+                });
+            }
+            else {
+                $("#fim-btn").show();
+                $("#loader-img").removeClass("loader");
+            }
+
+        },
+        error: function(xhr) {
+            alert("Ops, ocorreu um error: " + xhr.status + " - " + xhr.statusText);
+        },
+        complete: function() {
+            $("#loader-img").hide();
         }
-    })
+    });
 }
